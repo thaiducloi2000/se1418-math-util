f24bc71518ccde24dba3908b5a8ce1f69ea728f9
doit-now <hoang.nguyenthe@gmail.com>
2020-11-19 08:36:56 +0700

    use recursion and GREEN again

--- src/fu/util/MathUtility.java
+++ src/fu/util/MathUtility.java
@@ -17,6 +17,22 @@ public class MathUtility {
     //hàm tính n giai thừa. n >= 0 && n <= 20
     //nếu quá 20 tràn kiểu long
     //0! = 1; 1! = 1; n! = 1.2.3...n
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+//        //đưa đầu vào cà chớn, mình hok thèm trả về 1 giá ám chỉ sai rồi
+//        //mình chửi luôn
+//        if (n == 0 || n == 1)
+//            return 1; //0! = 1! = 1
+//        //sống sót đến đây, thì n > 1 và <= 20 rồi
+//        long result = 1;
+//        
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;    
+//    }
+    
     public static long getFactorial(int n) {
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("n must be >= 0 & <= 20");
@@ -24,14 +40,16 @@ public class MathUtility {
         //mình chửi luôn
         if (n == 0 || n == 1)
             return 1; //0! = 1! = 1
-        //sống sót đến đây, thì n > 1 và <= 20 rồi
-        long result = 1;
         
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;    
-    }
+        return n * getFactorial(n - 1); //đệ quy đó em
+                                        //gọi lại chính mình với quy mô
+                                        //nhỏ hơn
+                                        //5! = 5 * 4!
+                                        //4! = 4 * 3!
+                                        //3! = 3 * 2!
+                                        //2! = 2 * 1! //dừng  
+                                        //n! = n * (n - 1)!        
+    } //chơi lớn ko thèm check local, xem trực tiếp GitHub Action xanh
 }
 
 //Math.sqrt(),  .pow(a, b) a^b,  .abs() trị tuyệt đối, .sin()
